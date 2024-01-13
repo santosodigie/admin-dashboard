9e080dc699f4ef1f9db7e61cbdf17aec3a367977
Santos <D00233649@student.dkit.ie>
2024-01-13 12:32:39 +0000

    feat: implement Chart component for data visualization
    - Develop a reusable Chart component using 'recharts' library
    - Enable dynamic data display through props: title, data, dataKey, and grid
    - Implement a responsive design for adaptability across devices
    - Incorporate features like XAxis, Line, Tooltip, and optional CartesianGrid for enhanced data interpretation
    
    This commit adds a versatile and interactive charting solution to the application, facilitating effective visualization of various data sets.

---
+++ client/src/components/chart/Chart.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const Chart = ({ title, data, dataKey, grid }) => {
+  return (
+    <div className="chart m-5 p-5 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+      <h3 className="chartTitle mb-5 font-bold text-xl">{title}</h3>
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+        <LineChart data={data}>
+          <XAxis dataKey="name" stroke="#5550bd" />
+          <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
+          <Tooltip />
+          {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Chart;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import Chart from "../../components/chart/Chart";
+import { userData } from "../../dummyData";
 
 const Home = () => {
   return (
     <div className="home flex-[4]">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 };
